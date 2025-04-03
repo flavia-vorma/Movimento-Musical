dce63c746acb792080df0eff47cc3b2e6b9906d1
flavia-vorma <165107267+flavia-vorma@users.noreply.github.com>
2025-04-03 20:28:14 +0000

    Finalizado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  document.addEventListener('DOMContentLoaded' , function(){
+document.addEventListener('DOMContentLoaded' , function(){
     let tamanhoAtualFonte = 1;
 
     const botaoAumentarFonte = document.getElementById('aumentar-fonte');
