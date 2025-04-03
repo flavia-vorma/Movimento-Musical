6218f8e2d69a4691a94997a7d4084515892eabec
flavia-vorma <165107267+flavia-vorma@users.noreply.github.com>
2025-04-03 18:23:57 +0000

    03/04/2025

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-document.addEventListener('DOMContentLoaded', Function(){
-  let tamanhoAtualFonte = 1;
+  document.addEventListener('DOMContentLoaded' , function(){
+    let tamanhoAtualFonte = 1;
 
-  const botaoAumentarFonte = document.getElementById('aumentar-fonte');
-  const botaoAumentarFonte = document.getElementById('diminuir-fonte');
+    const botaoAumentarFonte = document.getElementById('aumentar-fonte');
+    const botaoDiminuirFonte = document.getElementById('diminuir-fonte');
 
-  botaoAumentarFonte.addEventListener('click', function(){
-    tamanhoAtualFonte += 0.1;
-   document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    
+    botaoAumentarFonte.addEventListener('click' , function(){
+     tamanhoAtualFonte += 0.1;
+     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    botaoDiminuirFonte.addEventListener('click' , function(){
+     tamanhoAtualFonte -= 0.1;
+     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
   });
-  
-});
